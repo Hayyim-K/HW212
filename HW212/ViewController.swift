167df65f4cb723929b60935d68c8a56193b128aa
Hayyim <b2banalytica@gmail.com>
2020-10-14 14:15:15 +0300

    Initial Commit

---
+++ HW212/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW212
+//
+//  Created by vitasiy on 14.10.2020.
+//  Copyright © 2020 b2banalytica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
