66e2cd4a38d5332dbdaf204885e78fc8649247de
Hayyim <b2banalytica@gmail.com>
2020-10-14 21:39:30 +0300

    lights

--- HW212/ViewController.swift
+++ HW212/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var buttonLable: UIButton!
+    
+    var lightType = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = redLightView.frame.height/2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.height/2
+        greenLightView.layer.cornerRadius = greenLightView.frame.height/2
+        
+        buttonLable.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func lightMotion() {
+        buttonLable.setTitle("NEXT", for: .normal)
+        switch lightType {
+        case 0:
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+            lightType += 1
+        case 1:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            lightType += 1
+            
+        default:
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            lightType = 0
+        }
+        
+        
+        
+    }
 }
 
